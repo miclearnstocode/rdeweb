4d455e2752e5fdcefb4cbc5a20219c5626675658
Michael <estorque.michael@student.capsu.edu.ph>
2026-06-03 22:26:32 +0800

    fix the unknown and redirect to login fix the recursive login problem.

--- client/index.js
+++ client/index.js
@@ -1,23 +1,13 @@
 import {$} from './lib/lib.js'
-
 import {LoginPage} from "./component/login.js";
-
 import {AdminPanel} from "./component/admin.js";
-
 import {UserPanel} from "./component/user.js";
-
 import {Error as ErrorPage} from "./error.js";
-
 import {Evaluator} from "./component/evaluator.js";
-
 import {RdeOffice} from "./component/rdeStaff/script/rde.js";
-
 import {Scanner} from "./component/scanner.js";
-
 import {DocumentViewer} from "./component/otherComponent/Document.js";
-
 import {Retrieval} from "./AccountRetrival/retrival.js";
-
 import {ResearchChairPanel} from "./component/researchChair.js";
 
 (function() {
@@ -32,15 +22,10 @@ import {ResearchChairPanel} from "./component/researchChair.js";
         return originalFetch.apply(this, args)
             .then(response => {
                 const duration = Date.now() - startTime;
-                
-                // Clone to check content without consuming
                 const clone = response.clone();
                 const contentType = clone.headers.get('content-type') || '';
-                
-                // Check if it's JSON or HTML
                 clone.text().then(text => {
                 }).catch(e => console.log('Could not read response text:', e));
-                
                 return response;
             })
             .catch(error => {
@@ -50,209 +35,172 @@ import {ResearchChairPanel} from "./component/researchChair.js";
 })();
 
 const Login = () => {
-
     return ($({
-
         tag: 'div',
-
-        child: [
-
-
-
-            LoginPage(),
-
-        ]
-
+        child: [LoginPage()]
     }))
-
 }
-
-
 
 const Admin = () => {
-
     return ($({
-
         tag: 'div',
-
-        child: [
-
-            AdminPanel(),
-
-        ]
-
+        child: [AdminPanel()]
     }))
-
 }
 
-
-
 const User = () => {
-
     return ($({
-
         tag: 'div',
-
-        child: [
-
-            UserPanel(),
-
-        ]
-
+        child: [UserPanel()]
     }))
-
 }
 
 const ResearchChair = () => {
-
     return ($({
         tag: 'div',
-        child: [
-
-            ResearchChairPanel(),
-        ]
+        child: [ResearchChairPanel()]
     }))
 }
 
 const Docs = () => {
     return ($({
-
         tag: 'div',
-
         text: 'hello this is docs'
-
     }))
-
 }
 
-
-
-
-
-
-
 const url = window.location.href.replace(window.location.origin, '')
-
 let Render
 
+// Mobile device check
 (function (a, b) {
-
     if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4)))
-
     {
-
         document.body.innerHTML=''
-
         alert("The system currently unavailable for mobile devices...")
-
     }
-
 })(navigator.userAgent || navigator.vendor || window.opera, 'http://detectmobilebrowser.com/mobile');
 
+// ========== SESSION PROTECTION FUNCTION ==========
+async function checkAndProtectSession() {
+    try {
+        const response = await fetch('/sessionCheck', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: 'sessionChecker=1'
+        });
+        
+        const data = await response.json();
+        
+        // Check if we need to redirect (status true means redirect)
+        if (data.status === true || data.redirect === true) {
+            console.log('Session invalid, redirecting to login...');
+            window.location.href = '/account/Login';
+            return false;
+        }
+        
+        // Also check for UNKNOWN username via getUserName
+        const userResponse = await fetch('/sessionCheck', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: 'getUserName=1'
+        });
+        
+        const userData = await userResponse.json();
+        
+        // If username is UNKNOWN or needs redirect, redirect to login
+        if (userData.redirect === true || userData.username === 'UNKNOWN' || userData.username === '') {
+            console.log('UNKNOWN username detected, redirecting to login...');
+            window.location.href = '/account/Login';
+            return false;
+        }
+        
+        return true;
+        
+    } catch (error) {
+        console.error('Session protection error:', error);
+        window.location.href = '/account/Login';
+        return false;
+    }
+}
 
-window.addEventListener('DOMContentLoaded', () => {
-
-    (function (url) {
-
+// ========== MAIN APPLICATION ==========
+window.addEventListener('DOMContentLoaded', async () => {
+    (async function(url) {
+        // Public routes - no session check needed
         if (url.split('/')[1] === 'account') {
-
+            // Login page
             document.body.appendChild($({
-
                 tag: 'div',
-
-                att: {
-
-                    id: 'particles-js'
-
-                },
-
+                att: { id: 'particles-js' },
                 style: {
-
                     height: '99%',
-
                     width: '100%',
-
                     position: 'absolute',
-
                     zIndex: '-1',
-
                 }
-
             }))
-
             document.body.appendChild($({
-
                 tag: 'script',
-
-                att: {
-
-                    src: '/particle/particles.js'
-
-                }
-
+                att: { src: '/particle/particles.js' }
             }))
-
             setTimeout(()=>{
-
                 document.body.appendChild($({
-
                     tag: 'script',
-
-                    att: {
-
-                        src: '/particle/app.js'
-
-                    }
-
+                    att: { src: '/particle/app.js' }
                 }))
-
             },100)
-
             Render = Login
-
             document.getElementById('root').appendChild(Render())
-
-        } else if (url.split('/')[1] === 'view') {
-
+            return;
+        } 
+        
+        // Public view route
+        if (url.split('/')[1] === 'view') {
             Render = DocumentViewer
-
             document.getElementById('root').appendChild(Render())
-
-        } else if (url.split('/')[1] === 'accountSupport'){
-
-            Render=Retrieval
-
-            document.getElementById('root').appendChild(Render())
-
+            return;
         }
-
-        else {
-
-            // Session check DISABLED - causes recursive redirects
-            // Trust server session management instead of checking on every page load
-            // Just render the appropriate component based on URL
-            
-            if (url.split('/')[1] === 'admin') {
-                Render = Admin
-            } else if (url.split('/')[1] === 'user') {
-                Render = User
-            } else if (url.split('/')[1] === 'research-chair') {
-                Render = ResearchChair
-            } else if (url.split('/')[1] === 'evaluator') {
-                Render = Evaluator
-            } else if (url.split('/')[1] === 'rdeOffice') {
-                Render = RdeOffice
-            } else if (url.split('/')[1] === 'researcher') {
-                Render = Evaluator
-            } else if (url.split('/')[1] === 'external') {
-                Render = RdeOffice
-            } else {
-                Render = ErrorPage
-            }
-            
+        
+        // Public account support route
+        if (url.split('/')[1] === 'accountSupport'){
+            Render = Retrieval
             document.getElementById('root').appendChild(Render())
-
+            return;
         }
-
+        
+        // ===== PROTECTED ROUTES - Check session first =====
+        const isSessionValid = await checkAndProtectSession();
+        
+        // If session is invalid, checkAndProtectSession already redirected
+        if (!isSessionValid) {
+            return;
+        }
+        
+        // Session is valid, render the appropriate component
+        if (url.split('/')[1] === 'admin') {
+            Render = Admin
+        } else if (url.split('/')[1] === 'user') {
+            Render = User
+        } else if (url.split('/')[1] === 'research-chair') {
+            Render = ResearchChair
+        } else if (url.split('/')[1] === 'evaluator') {
+            Render = Evaluator
+        } else if (url.split('/')[1] === 'rdeOffice') {
+            Render = RdeOffice
+        } else if (url.split('/')[1] === 'researcher') {
+            Render = Evaluator
+        } else if (url.split('/')[1] === 'external') {
+            Render = RdeOffice
+        } else {
+            Render = ErrorPage
+        }
+        
+        document.getElementById('root').appendChild(Render())
+        
     })(window.location.href.replace(window.location.origin, ''))
 })
--- server/session.php
+++ server/session.php
@@ -1,5 +1,5 @@
 <?php
-// Start output buffering FIRST to catch any notices/warnings
+// Start output buffering FIRST
 ob_start();
 
 // Check if session is already started before starting it
@@ -7,63 +7,132 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
+// ===== CRITICAL: Check for UNKNOWN username FIRST - BEFORE ANYTHING ELSE =====
+// This runs on EVERY request and immediately destroys invalid sessions
+if (isset($_SESSION['userName']) && $_SESSION['userName'] === 'UNKNOWN') {
+    // Clear invalid session
+    $_SESSION = array();
+    if (ini_get("session.use_cookies")) {
+        $params = session_get_cookie_params();
+        setcookie(session_name(), '', time() - 42000,
+            $params["path"], $params["domain"],
+            $params["secure"], $params["httponly"]
+        );
+    }
+    session_destroy();
+    
+    // Redirect to login immediately
+    header('Location: /account/Login');
+    exit();
+}
+
+// Also check for empty or null username
+if (!isset($_SESSION['userName']) || empty($_SESSION['userName']) || $_SESSION['userName'] === '') {
+    // Don't set UNKNOWN - just redirect if trying to access protected content
+    if (!isset($_SERVER['HTTP_X_REQUESTED_WITH']) || 
+        strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest') {
+        header('Location: /account/Login');
+        exit();
+    }
+}
+
 include ('db.php');
 
-/** @var TYPE_NAME $host */
-/** @var TYPE_NAME $username */
-/** @var TYPE_NAME $pass */
-/** @var TYPE_NAME $dbName */
-
-// Set JSON header
-header('Content-Type: application/json');
-$response = new stdClass();
-
+// Set JSON header for AJAX requests
+if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && 
+    strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+    header('Content-Type: application/json');
+} else {
+    header('Content-Type: application/json');
+}
 
 if(isset($_POST['sessionChecker'])){
-    // FIXED: Check login status correctly this cause that the user is not logged in but the session is still active. so when using OR condition we need to check if the session is still active. So use AND condition instead of OR condition.
-    if(isset($_SESSION['login']) && $_SESSION['login'] === true){
-        // User IS logged in - don't redirect
-        $response->status = false;  // false = don't redirect
+    $response = new stdClass();
+    
+    // Check for valid username AND valid login
+    $hasValidUsername = isset($_SESSION['userName']) && 
+                        $_SESSION['userName'] !== 'UNKNOWN' && 
+                        $_SESSION['userName'] !== '' && 
+                        $_SESSION['userName'] !== null;
+    
+    $isValidLogin = isset($_SESSION['login']) && 
+                    $_SESSION['login'] === true && 
+                    $hasValidUsername;
+    
+    if($isValidLogin){
+        $response->status = false;
+        $response->redirect = false;
         $response->message = $_SESSION['userType'] ?? '';
         $response->username = $_SESSION['userName'] ?? '';
         $response->userId = $_SESSION['userId'] ?? '';
     } else {
-        // User is NOT logged in - redirect to login
-        $response->status = true;  // true = redirect to login
-        $response->message = 'Not logged in';
-        $response->sessionId = session_id();
+        $response->status = true;
+        $response->redirect = true;
+        $response->message = 'Invalid session - please login again';
+        $response->redirectUrl = '/account/Login';
+        
+        // Clear invalid session
+        if(isset($_SESSION['login'])) {
+            $_SESSION = array();
+            if (ini_get("session.use_cookies")) {
+                $params = session_get_cookie_params();
+                setcookie(session_name(), '', time() - 42000,
+                    $params["path"], $params["domain"],
+                    $params["secure"], $params["httponly"]
+                );
+            }
+            session_destroy();
+        }
     }
     
-    // Clear buffer and output JSON
     ob_clean();
     echo json_encode($response);
     ob_end_flush();
     exit();
 }
 
-if(isset($_POST['getUser'])){
-    ob_clean();
-    echo json_encode([
-        'designation' => $_SESSION['userDesignation'] ?? 'UNKNOWN'
-    ]);
-    ob_end_flush();
-    exit();
-}
-
 if(isset($_POST['getUserName'])){
+    // NEVER return 'UNKNOWN' - check and redirect instead
+    $hasValidUsername = isset($_SESSION['userName']) && 
+                        $_SESSION['userName'] !== 'UNKNOWN' && 
+                        $_SESSION['userName'] !== '' &&
+                        $_SESSION['userName'] !== null;
+    
+    if($hasValidUsername && isset($_SESSION['login']) && $_SESSION['login'] === true){
+        $response = [
+            'username' => $_SESSION['userName'],
+            'redirect' => false
+        ];
+    } else {
+        // Instead of returning 'UNKNOWN', force redirect
+        $response = [
+            'username' => '',
+            'redirect' => true,
+            'redirectUrl' => '/account/Login',
+            'message' => 'Session invalid - please login'
+        ];
+        
+        // Clear the invalid session
+        if (session_status() === PHP_SESSION_ACTIVE) {
+            $_SESSION = array();
+            session_destroy();
+        }
+    }
+    
     ob_clean();
-    echo json_encode([
-        'username' => $_SESSION['userName'] ?? 'UNKNOWN'
-    ]);
+    echo json_encode($response);
     ob_end_flush();
     exit();
 }
 
-// Default response if no valid POST data
+// Default response
 ob_clean();
 echo json_encode([
     'error' => 'Invalid request',
-    'status' => false
+    'status' => true,
+    'redirect' => true,
+    'redirectUrl' => '/account/Login',
+    'message' => 'Please login to continue'
 ]);
 ob_end_flush();
 exit();
