bf817de680b948ae60a549f7e36d4cce51793e06
Michael <estorque.michael@student.capsu.edu.ph>
2026-07-30 09:59:28 +0800

    fix the certificates

--- client/component/rdeStaff/script/src/entrySummary.js
+++ client/component/rdeStaff/script/src/entrySummary.js
@@ -3256,6 +3256,8 @@ export const Content = (mainFrame, leftPDiv = null) => {
                                     position: absolute;
                                     top: 0;
                                     left: 0;
+                                    right: 0;
+                                    bottom:0;
                                     width: 100%;
                                     height: 100%;
                                     z-index: 1;
